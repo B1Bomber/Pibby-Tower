88ab258edf0498065bbccfe287e23d485773cd12
B2Bomber2 <ferrariguan@HP-Laptop-15-MacOS.local>
2025-05-30 17:05:49 -0700

    2 levels done

--- main.py
+++ main.py
@@ -27,10 +27,11 @@ if __name__ == "__main__":
     # Load images
     load_player_img = pygame.image.load('assets/plantson.png').convert_alpha()
     load_boss_img = pygame.image.load('assets/Castle_Pibby.png').convert_alpha()
-
+    load_boss_img2 = pygame.image.load('assets/PIBBY_snail.png').convert_alpha()
     # Scale them to fit
     player_img = pygame.transform.smoothscale(load_player_img, (80, 80))
     boss_img = pygame.transform.smoothscale(load_boss_img, (80, 80))
+    boss_img2 = pygame.transform.smoothscale(load_boss_img2, (80, 80))
 
     player_pos = [
         screenWidth / 2 - player_img.get_width() / 2,
@@ -53,8 +54,51 @@ def draw_health_bars(surface, x, y, bossHealth, playerHealth):
     pygame.draw.rect(surface, RED, (x, screenHeight - healthBarHeight - y, 200, healthBarHeight))
     pygame.draw.rect(surface, GREEN, (x, screenHeight - healthBarHeight - y, 2 * playerHealth, healthBarHeight))
 
+def boss_actions(x, y, minX, maxX, minY, maxY, speed, bullets):
+    moveRight = x + 2 * speed
+    moveLeft = x - 2 * speed
+    moveUp = y - 2 * speed
+    moveDown = y + 2 * speed
+
+    dangerMap = {}
+    dangerMap["rightDanger"] = 0
+    dangerMap["leftDanger"] = 0
+    dangerMap["upDanger"] = 0
+    dangerMap["downDanger"] = 0
+    
+    if moveRight > maxX:
+        dangerMap["rightDanger"] = float('inf')
+    if moveLeft < minX:
+        dangerMap["leftDanger"] = float('inf')
+    if moveUp < minY:
+        dangerMap["upDanger"] = float('inf')
+    if moveDown > maxY:
+        dangerMap["downDanger"] = float('inf')
+
+    for bullet in bullets[:]:
+        if x <= bullet.x <= moveRight:
+            dangerMap["rightDanger"] += 1
+        if moveLeft <= bullet.x <= x:
+            dangerMap["leftDanger"] += 1
+        if moveUp <= bullet.y <= y:
+            dangerMap["upDanger"] += 1
+        if y <= bullet.y <= moveDown:
+            dangerMap["downDanger"] += 1
+
+    # add clamp conditions to prevent corner hugs
+    # if a move is out of bounds, set the danger level to infinity
+
+    safestMove = min(dangerMap, key=dangerMap.get)
+    if dangerMap[safestMove] == 0:
+        return str(0)
+    return safestMove
+    
+
 class Bullet:
-    def __init__(self, x, y, speed, state):
+    def __init__(self, x, y, origin, speed, state):
+        self.state = state
+        self.origin = origin
+        # self.rect = pygame.Rect(self.x - self.radius, self.y - self.radius, self.radius * 2, self.radius * 2)
         if (state == "super"):
             self.x = x
             self.y = y
@@ -62,13 +106,15 @@ class Bullet:
             self.color = (255, 255, 0)
             self.speed = speed * 2
             self.alive = True
+            self.damage = 15
         elif (state == "random"): 
-            self.x = random.randint(-255, 255)
-            self.y = random.randint(-255, 255)
+            self.x = x
+            self.y = y
             self.radius = random.randint(1, 10)
             self.color = (0, 255, 255)
-            self.speed = random.randint(-100, 100)
+            self.speed = 0
             self.alive = True
+            self.damage = random.randint(0, 25)
         else: 
             self.x = x
             self.y = y
@@ -76,11 +122,23 @@ class Bullet:
             self.color = (255, 0, 255)
             self.speed = speed
             self.alive = True
+            self.damage = 5 
 
 
     def update(self):
-        self.y -= self.speed
-        if (self.y < 0) or (self.y > screenHeight):
+        if (self.state == "random") and (self.origin == "player"):
+            self.y += random.randint(-10, 10)
+            self.x += random.randint(-10, 10)
+        elif (self.state == "random") and (self.origin == "boss"):
+            self.y += random.randint(0, 10)
+            self.x += random.randint(-10, 10)
+        else:
+            if (self.origin == "player"):
+                self.y -= self.speed
+            elif (self.origin == "boss"):
+                self.y += self.speed
+            
+        if (self.y < 0) or (self.y > screenHeight) or (self.x < 0) or (self.x > screenWidth):
             self.alive = False
 
     def draw(self, surface):
@@ -90,12 +148,15 @@ class Bullet:
 # finite state machine
 running = True
 gameOver = False
-tutorial = True
-levelOne = False
+tutorial = False
+levelOne = True
 levelTwo = False
 
 # attack cool down
 last_shot_time = 0 
+boss_shot_time = 0
+super_boss_shot_time = 0
+random_boss_shot_time = 0
 shoot_cooldown = 700  # milliseconds, 0.3 seconds
 
 # Game loop
@@ -120,9 +181,13 @@ while running:
             player_pos[1] -= 2
         if keys[pygame.K_DOWN]:
             player_pos[1] += 2
-        if keys[pygame.K_SPACE] and current_time - last_shot_time > shoot_cooldown:
-            bullet = Bullet(player_pos[0] + player_img.get_width() // 2, player_pos[1], 2, "")
+        if keys[pygame.K_SPACE] and (current_time - last_shot_time > shoot_cooldown):
+            bullet = Bullet(player_pos[0] + player_img.get_width() // 2, player_pos[1], "player", 2, "")
             bullets.append(bullet)
+            last_shot_time = current_time
+        if keys[pygame.K_c] and (current_time - last_shot_time > (shoot_cooldown * 2)):
+            superBullet = Bullet(player_pos[0] + player_img.get_width() // 2, player_pos[1], "player", 2, "super")
+            bullets.append(superBullet)
             last_shot_time = current_time
 
         # Clamp position
@@ -130,19 +195,20 @@ while running:
         player_pos[1] = max(healthBarHeight, min(player_pos[1], screenHeight - player_img.get_height() - healthBarHeight))
 
         # boss shooting
-        # if random.randint(0, 20) == 0:
-        #     bullets.append([boss_pos[0] + boss_img.get_width() // 2, boss_pos[1] + boss_img.get_height()])
+        if random.randint(0, 100) == 0:
+            bossBullet = Bullet(boss_pos[0] + boss_img.get_width() // 2, boss_pos[1] + boss_img.get_height(), "boss", 2, "")
+            bullets.append(bossBullet)
 
         # Update bullets
-        # for bullet in bullets[:]:
-        #     bullet[1] += 3
-        #     elif player_img.get_rect(topleft=player_pos).collidepoint(bullet):
-        #         bullets.remove(bullet)
-        #         player_health -= 5
-
-        for bullet in bullets[:]:  # iterate over a copy to allow safe removal
+        for bullet in bullets[:]:
             bullet.update()
             bullet.draw(screen)
+            if player_img.get_rect(topleft=player_pos).collidepoint(bullet.x, bullet.y):
+                bullet.alive = False
+                player_health -= bullet.damage
+            if boss_img.get_rect(topleft=boss_pos).collidepoint(bullet.x, bullet.y):
+                bullet.alive = False
+                boss_health -= bullet.damage
             if not bullet.alive:
                 bullets.remove(bullet)
 
@@ -160,6 +226,95 @@ while running:
         pygame.display.flip()
         clock.tick(60)
     
+    elif not gameOver and levelOne:
+        # player control
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player_pos[0] -= 2
+        if keys[pygame.K_RIGHT]:
+            player_pos[0] += 2
+        if keys[pygame.K_UP]:
+            player_pos[1] -= 2
+        if keys[pygame.K_DOWN]:
+            player_pos[1] += 2
+        if keys[pygame.K_SPACE] and (current_time - last_shot_time > shoot_cooldown):
+            bullet = Bullet(player_pos[0] + player_img.get_width() // 2, player_pos[1], "player", 2, "")
+            bullets.append(bullet)
+            last_shot_time = current_time
+        if keys[pygame.K_c] and (current_time - last_shot_time > (shoot_cooldown * 2)):
+            superBullet = Bullet(player_pos[0] + player_img.get_width() // 2, player_pos[1], "player", 2, "super")
+            bullets.append(superBullet)
+            last_shot_time = current_time
+        
+
+        # Clamp position
+        player_pos[0] = max(0, min(player_pos[0], screenWidth - player_img.get_width()))
+        player_pos[1] = max(healthBarHeight, min(player_pos[1], screenHeight - player_img.get_height() - healthBarHeight))
+
+        # boss move
+        bossMovement = boss_actions(boss_pos[0], boss_pos[1], 0, min(boss_pos[0], screenWidth - boss_img.get_width()), healthBarHeight, min(boss_pos[1], screenHeight - boss_img.get_height() - healthBarHeight), 2, bullets)
+        if (bossMovement == "leftDanger"):
+            boss_pos[0] -= 2
+        elif (bossMovement == "rightDanger"):
+            boss_pos[0] += 2
+        elif (bossMovement == "upDanger"):
+            boss_pos[1] -= 2
+        elif (bossMovement == "upDanger"):
+            boss_pos[1] += 2
+
+        # boss shooting
+        if current_time - boss_shot_time >= shoot_cooldown:
+            bossBullet = Bullet(boss_pos[0] + boss_img.get_width() // 2, boss_pos[1] + boss_img.get_height(), "boss", 2, "")
+            bullets.append(bossBullet)
+            boss_shot_time = current_time
+        if current_time - super_boss_shot_time >= (shoot_cooldown * 2):
+            superBossBullet = Bullet(boss_pos[0] + boss_img.get_width() // 2, boss_pos[1] + boss_img.get_height(), "boss", 2, "super")
+            bullets.append(superBossBullet)
+            super_boss_shot_time = current_time
+        if current_time - random_boss_shot_time >= (shoot_cooldown * 3):
+            randomBossBullet = Bullet(boss_pos[0] + boss_img.get_width() // 2, boss_pos[1] + boss_img.get_height(), "boss", 2, "random")
+            bullets.append(randomBossBullet)
+            random_boss_shot_time = current_time
+
+        # Update bullets
+        for bullet in bullets[:]:
+            bullet.update()
+            bullet.draw(screen)
+            if player_img.get_rect(topleft=player_pos).collidepoint(bullet.x, bullet.y):
+                bullet.alive = False
+                player_health -= bullet.damage
+            if boss_img.get_rect(topleft=boss_pos).collidepoint(bullet.x, bullet.y):
+                bullet.alive = False
+                boss_health -= bullet.damage
+            if not bullet.alive:
+                bullets.remove(bullet)
+
+        # Draw
+        screen.blit(player_img, player_pos)
+        screen.blit(boss_img, boss_pos)
+        draw_health_bars(screen, 10, 10, boss_health, player_health)
+
+        if player_health <= 0:
+            gameOver = True
+        elif boss_health <= 0:
+            player_pos = [
+                screenWidth / 2 - player_img.get_width() / 2,
+                screenHeight * 0.75 - player_img.get_height() / 2
+            ]
+
+            boss_pos = [
+                screenWidth / 2 - boss_img.get_width() / 2,
+                screenHeight * 0.25 - boss_img.get_height() / 2
+            ]
+            bullets = []
+            player_health = 100
+            boss_health = 200
+            levelOne = False
+            levelTwo = True
+
+        pygame.display.flip()
+        clock.tick(60)
+
     elif gameOver:
         gameOverText = font.render('Game Over', True, RED)
         screen.blit(gameOverText, gameOverText.get_rect(center=(screenWidth / 2, screenHeight / 2)))
